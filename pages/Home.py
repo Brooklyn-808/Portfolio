84735a10f859b63675293e80dcf75495bd66acfe
Brooklyn 808 <142480789+Brooklyn-808@users.noreply.github.com>
2024-05-19 19:49:56 +0800

    Update Home.py

--- pages/Home.py
+++ pages/Home.py
@@ -1,7 +1,6 @@
 import streamlit as st
 
+st.sidebar.page_link("About Me.py", label="About")
 st.title("Welcome to My Portfolio!")
 st.write("This is the home page of my portfolio website.")
 st.write("Feel free to browse around and learn more about me.")
-st.sidebar.title("Navigation")
-st.sidebar.page_link("About Me.py", label="About")
