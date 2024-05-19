5c123bd0d125e123b588e2ece06bf4cdafe69c73
Brooklyn 808 <142480789+Brooklyn-808@users.noreply.github.com>
2024-05-19 19:52:03 +0800

    Delete pages/About Me.py

--- pages/About Me.py
+++
@@ -1,5 +0,0 @@
-import streamlit as st
-
-st.title("About Me")
-st.write("I am a passionate developer who loves creating awesome projects.")
-st.write("In my free time, I enjoy learning new technologies and building cool stuff.")
