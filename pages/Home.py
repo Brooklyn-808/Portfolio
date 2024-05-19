2c6c1df02bb098f8d67161bc853365f7e0d21e22
Brooklyn 808 <142480789+Brooklyn-808@users.noreply.github.com>
2024-05-19 19:53:04 +0800

    Update Home.py

--- pages/Home.py
+++ pages/Home.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.page_link("pages/About Me.py", label="About")
+st.page_link("About.py", label="About")
 st.title("Welcome to My Portfolio!")
 st.write("This is the home page of my portfolio website.")
 st.write("Feel free to browse around and learn more about me.")
